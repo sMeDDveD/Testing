d3c3d7bf9727be23da653475154d07f60614ce0b
maliauaks <maliaukas@gmail.com>
2019-11-01 20:49:41 +0300

    Order changed

--- Testing/src/Fourth/Malyavko_Soloduha_Test.java
+++ Testing/src/Fourth/Malyavko_Soloduha_Test.java
@@ -2,6 +2,7 @@ package Fourth;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,17 +20,22 @@ public class Malyavko_Soloduha_Test {
     }
 
     @Test
+    @Order(3)
     public void test1() {
         System.out.println("I am test №1!");
     }
 
     @Test
+    @Order(2)
     public void test2() {
         assertEquals(2, 1 + 1);
+        System.out.println("I am test №2!");
     }
 
     @Test
+    @Order(1)
     public void test3() {
         fail("Fail!");
     }
+
 }
